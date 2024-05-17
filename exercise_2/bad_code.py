d178f8bd6ec7d0352ec211da23251a388f8b83fc
gregorycottone Cottone <gregory.cottone@yahoo.it>
2024-05-17 08:48:59 +0000

    lint_and_test yaml file corrected again

--- exercise_2/bad_code.py
+++ exercise_2/bad_code.py
@@ -1,13 +1,9 @@
-import os  # noqa: F401
+import os
 
 def basic_math():
     return 2 + 3
 
-
-# A comment that is so long, it is going to be split into multiple lines due for general readability purposes
 if __name__ == "__main__":
     hello = "Hello, World!"
 
     basic_math()
-
-    # Added an extra blank line for readability
